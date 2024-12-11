644ff9740f5e0140aeee9a967df4c17aa749e210
Ayush Kumar <socialacc211@gmail.com>
2024-12-12 03:04:07 +0530

    debounce & cookie

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,63 @@
 
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
 const axiosClient = axios.create({
   baseURL: 'http://localhost:3000',
-  
+
 
 })
 
 function App() {
-  useEffect( ()=>{
-    (async ()=>{
-      axiosClient.get('/test',{
+  const [input, setInput] = useState("");
+  const idRef = useRef<null | number>(null);
+  const [data, setData ] = useState(null)
+  const countRef = useRef(0);
+  useEffect(() => {
+    (async () => {
+      await axiosClient.get('/test', {
         params: {
           id: 'aa'
         },
         timeout: 10000,
         headers: {
           'h1': 'h1-value',
-          'Origin2': 'ayushkumar.club'
-        }
+          'Origin2': 'ayushkumar.club',
+          Authorization: "Bearer true",
+        },
+        withCredentials: true
       })
     })()
-  })
+  }, [])
+
+  countRef.current++;
+  console.log(countRef.current)
+
+  // debouncing
+  useEffect(() => {
+    (async () => {
+      if(idRef.current) clearTimeout(idRef.current);      
+      idRef.current = setTimeout(async () => {
+        console.log("input", input)
+        const temp = await axiosClient.post('/search', { input }, {
+          headers: {
+            "Content-Type": 'application/json'
+          }
+        })
+        setData(temp.data)
+        console.log(temp)
+      },1000)
+
+    })()
+  }, [input])
 
   return (
 
     <>
       <h1>hi</h1>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <input type='text' value={data || ""} readOnly/>
     </>
   )
 }
--- server/src/touch.js
+++ server/src/touch.js
@@ -2,27 +2,53 @@ import e from 'express'
 import cors from 'cors'
 import fs from 'fs'
 import { stringify } from 'querystring'
-
+import cookieParser from 'cookie-parser'
 const app = e()
 
+app.use(cookieParser())
+app.use(e.urlencoded({ extended: true }))
+app.use(e.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
-    // credentials: true
-    allowedHeaders: ['h1','origin2'],
-    methods: ['POST','TEMP'],
-    maxAge: 5000,
-    exposedHeaders: false
+  origin: ['http://localhost:5173', 'http://vercel.com'],
+  // credentials: true
+  allowedHeaders: ['h1', 'origin2', 'Authorization', 'Content-Type'],
+  methods: ['POST', 'TEMP'],
+  credentials: true,
+  maxAge: 0,
+  // maxAge: 3600, // sets age of preflight request , means after first success preflight , will not req preflight for next <time> duration.
+  optionsSuccessStatus: 200,
+  exposedHeaders: true
 }))
 
-app.get('/test', async (req,res)=>{
-     fs.writeFile('./req.txt',stringify(req),{encoding: 'utf-8'},(err)=>[
-        console.log(err)
-    ])
-    res.setHeader('testFromBackend','testing')
-    return res.send("/test")
-    
+app.get('/test', async (req, res) => {
+  //  fs.writeFile('./req.txt',stringify(req),{encoding: 'utf-8'},(err)=>[
+  //     console.log(err)
+  // ])
+  // console.dir(req)
+  res.setHeader('test-From-Backend', 'testing')
+  res.cookie('test', 'value', {
+    expires: new Date( Date.now() + 24 * 60 * 60 * 1000), // 1 day expiry 
+    domain: 'localhost',
+    secure: true,
+    sameSite: 'none',
+    path: '/',
+
+  })
+  // res.setHeader('Expires',new Date().getTime())
+  res.setHeader('Cache-Control', 'no-store no-cache, must-revalidate, max-age=0');
+  res.setHeader('Expires', new Date(new Date() + (60 * 60 * 1000)).toUTCString())
+  return res.send("/test")
+  // res.setHeader('Location','http://localhost:5173/redirecting')
+  // return res.status(301).send('redirecting to google...')
+
 })
 
-app.listen(3000,()=>{
-    console.log('server is listening on port 3000')
+app.post('/search', async (req, res) => {
+  const { input } = req.body
+  console.log(input)
+  res.send(input)
+})
+
+app.listen(3000, () => {
+  console.log('server is listening on port 3000')
 })
